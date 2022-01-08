0680e4e01332ffff6c1d2db4f881dfa6fb99c2e9
halilhurmuz <halilhurmuz@yandex.com>
2022-01-08 23:08:48 +0300

    unnecessary files delete

--- dragAndDropComponent/src/preload.js
+++
@@ -1,7 +0,0 @@
-const {ipcRenderer,contextBridge} = require("electron");
-
-const api = {
-    isFile: (path) => ipcRenderer.invoke("is-file",path),
-}
-
-contextBridge.exposeInMainWorld("api",api);
